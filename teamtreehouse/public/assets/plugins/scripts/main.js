dbf11d8aef1421da58c652ec5aac43d971d80b8f
Adrian Tolero <91657952+adriantolero@users.noreply.github.com>
2021-10-22 11:32:54 +0800

    Second Commit Label

--- teamtreehouse/public/assets/plugins/scripts/main.js
+++ teamtreehouse/public/assets/plugins/scripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     
     $(".homepage-qa-border").click(function () {
-        $(this).find(".rotate").toggleClass("down"); //puta naa raman diay Github update in Test branch first commit
+        $(this).find(".rotate").toggleClass("down"); //puta naa raman diay Github update in Test branch second commit
     });
 });
